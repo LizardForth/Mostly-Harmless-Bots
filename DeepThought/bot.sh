6aea7da60978b71b40f5f45fd57107818637efd4
SpicyForthProgrammer <aagoldberg24@gmail.com>
2022-04-16 18:48:04 -0400

    Ok im pretty sure this one is the last parity change

--- DeepThought/bot.sh
+++ DeepThought/bot.sh
@@ -6,16 +6,16 @@ case $OS in
 	'Linux')
 		git fetch origin
 		git reset --hard origin/main
- 		make 
+ 		make clean
+ 		make make -j$(nproc)
 		./bot
 		;;
 	'NetBSD')
 		git fetch origin
                 git reset --hard origin/main
-		make -f Makefile.bsd 
+		make -f Makefile.bsd clean 
+		make -f Makefile.bsd -j$(nproc)
 		export LD_LIBRARY_PATH=/usr/pkg/lib
-		make -f Makefile.bsd clean
-		make -f Makefile.bsd 
 		./bot
 		;;
 esac	
