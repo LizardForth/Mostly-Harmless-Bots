5a0eb6fa8b22c6e14b74272c34b1192b2ae57c15
aaronGoldberg02 <aagoldberg24@gmail.com>
2022-12-18 20:56:25 -0500

    Added some useful comments

--- CockBot/cockbot.h
+++ CockBot/cockbot.h
@@ -1,16 +1,20 @@
+// Value's
+// *********************
+
 #define MUTE_ROLE 965854189517406238
-#define MUTE_TIME 60000
 
 #define GUILD_ID 953769673634246757
 
-#define BOT_STATUS "with FORTH"
+#define BOT_STATUS "with FORTH" // Will display next to name
 #define BOT_DETAILS "AAAAH"
 
 #define ADMIN_ROLE 953785894656147566
 
-#define POLLO_ID 953769673634246757
+#define POLLO_ID 953769673634246757 // Emoji ID are fairly annoying to get.
 
-#define COLOR_SUCCESS 4835913 
-#define COLOR_FALIURE 16077157
+                               // Convert regular rgb hex to decimal.
+#define COLOR_SUCCESS 4835913  // #49CA49
+#define COLOR_FALIURE 16077157 // #F55165 
 
-#define WATCHCAT_TIME 10 
+#define WATCHCAT_TIME 10 // In seconds
+                         // If a given command takes longer than this it will be automatically killed!
