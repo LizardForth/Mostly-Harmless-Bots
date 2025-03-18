99ee6e911d1873b6c1abed8c09950deab0e5ebca
Oliver Webb <oliverwebb8192@gmail.com>
2025-03-17 21:24:18 -0500

    Revert "Merge pull request #5 from unsignedup/add_concord_subproject"
    This reverts commit 688685b60fb44d418e3d8cdf70f25e6b78c6f474, reversing
    changes made to eb6e8b839532d0cfc68878b2f6eb3c7671423a7a.

--- CockBot/discordEvents.h
+++ CockBot/discordEvents.h
@@ -1,7 +1,7 @@
 #ifndef EVENTS_H_INCLUDED
 #define EVENTS_H_INCLUDED
 
-#include <discord.h>
+#include <concord/discord.h>
 
 void disOnReady(struct discord *bot_client, const struct discord_ready *event);
 
