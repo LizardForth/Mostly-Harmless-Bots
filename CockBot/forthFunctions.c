e9b1dbaa801399be4e09c01ea55ad2f72bc0a20a
Oliver Webb <oliverwebb8192@gmail.com>
2025-03-19 18:58:54 -0500

    Refactor forthFunctions.c

--- CockBot/forthFunctions.c
+++ CockBot/forthFunctions.c
@@ -92,14 +92,28 @@ void disLoadScript(ficlVm *forth_vm) {
   return;
 }
 
-void disUnOhio(ficlVm *forth_vm) {
+static struct discord *get_client_from_forth(ficlVm *forth_vm) {
   const struct discord_message *dis_msg;
   struct discord *bot_client;
   ficlDictionary *forth_dict = ficlVmGetDictionary(forth_vm);
   ficlString tempString1;
   FICL_STRING_SET_FROM_CSTRING(tempString1, "bot_client");
   ficlVmExecuteWord(forth_vm, ficlDictionaryLookup(forth_dict, tempString1));
-  bot_client = ficlStackPopPointer(forth_vm->dataStack);
+  return ficlStackPopPointer(forth_vm->dataStack);
+}
+
+static const struct discord_message *get_dis_msg_from_forth(ficlVm *forth_vm) {
+  struct discord_message *dis_msg;
+  ficlDictionary *forth_dict = ficlVmGetDictionary(forth_vm);
+  ficlString tempString2;
+  FICL_STRING_SET_FROM_CSTRING(tempString2, "bot_client");
+  ficlVmExecuteWord(forth_vm, ficlDictionaryLookup(forth_dict, tempString2));
+  return ficlStackPopPointer(forth_vm->dataStack);
+}
+
+void disUnOhio(ficlVm *forth_vm) {
+  const struct discord_message *dis_msg;
+  struct discord *bot_client = get_client_from_forth(forth_vm);
   uint64_t dis_userid = ficlStackPopInteger(forth_vm->dataStack);
   log_info("Muting: %lu", dis_userid);
   discord_remove_guild_member_role(bot_client, GUILD_ID, dis_userid, MUTE_ROLE,
@@ -109,47 +123,31 @@ void disUnOhio(ficlVm *forth_vm) {
 
 void disOhio(ficlVm *forth_vm) {
   const struct discord_message *dis_msg;
-  struct discord *bot_client;
-  ficlDictionary *forth_dict = ficlVmGetDictionary(forth_vm);
-  ficlString tempString1;
-  FICL_STRING_SET_FROM_CSTRING(tempString1, "bot_client");
-  ficlVmExecuteWord(forth_vm, ficlDictionaryLookup(forth_dict, tempString1));
-  bot_client = ficlStackPopPointer(forth_vm->dataStack);
+  struct discord *bot_client = get_client_from_forth(forth_vm);
   uint64_t dis_userid = ficlStackPopInteger(forth_vm->dataStack);
   log_info("Muting: %lu", dis_userid);
   discord_add_guild_member_role(bot_client, GUILD_ID, dis_userid, MUTE_ROLE,
                                 NULL, NULL);
 }
 
-void disKick(ficlVm *forth_vm) {
+void disUptime(ficlVm *forth_vm) {
   const struct discord_message *dis_msg;
-  struct discord *bot_client;
-  ficlDictionary *forth_dict = ficlVmGetDictionary(forth_vm);
-  ficlString tempString1;
-  ficlString tempString2;
-  FICL_STRING_SET_FROM_CSTRING(tempString1, "dis_msg");
-  FICL_STRING_SET_FROM_CSTRING(tempString2, "bot_client");
-  ficlVmExecuteWord(forth_vm, ficlDictionaryLookup(forth_dict, tempString2));
-  ficlVmExecuteWord(forth_vm, ficlDictionaryLookup(forth_dict, tempString1));
-  dis_msg = ficlStackPopPointer(forth_vm->dataStack);
-  bot_client = ficlStackPopPointer(forth_vm->dataStack);
+  struct discord *bot_client = get_client_from_forth(forth_vm);
+  uint64_t dis_userid = ficlStackPopInteger(forth_vm->dataStack);
+  log_info("Printing uptime", dis_userid);
+}
+
+void disKick(ficlVm *forth_vm) {
+  const struct discord_message *dis_msg = get_client_from_forth(forth_vm);
+  struct discord *bot_client = get_dis_msg_from_forth(forth_vm);
   uint64_t dis_userid = ficlStackPopInteger(forth_vm->dataStack);
   log_info("Kicking: %lu", dis_userid);
   discord_remove_guild_member(bot_client, GUILD_ID, dis_userid, NULL, NULL);
 }
 
 void disBan(ficlVm *forth_vm) {
-  const struct discord_message *dis_msg;
-  struct discord *bot_client;
-  ficlDictionary *forth_dict = ficlVmGetDictionary(forth_vm);
-  ficlString tempString1;
-  ficlString tempString2;
-  FICL_STRING_SET_FROM_CSTRING(tempString1, "dis_msg");
-  FICL_STRING_SET_FROM_CSTRING(tempString2, "bot_client");
-  ficlVmExecuteWord(forth_vm, ficlDictionaryLookup(forth_dict, tempString2));
-  ficlVmExecuteWord(forth_vm, ficlDictionaryLookup(forth_dict, tempString1));
-  dis_msg = ficlStackPopPointer(forth_vm->dataStack);
-  bot_client = ficlStackPopPointer(forth_vm->dataStack);
+  const struct discord_message *dis_msg = get_dis_msg_from_forth(forth_vm);
+  struct discord *bot_client = get_client_from_forth(forth_vm);
   uint64_t dis_userid = ficlStackPopInteger(forth_vm->dataStack);
   log_info("Ban: %lu", dis_userid);
   discord_create_guild_ban(
@@ -158,17 +156,8 @@ void disBan(ficlVm *forth_vm) {
 }
 
 void disDelBan(ficlVm *forth_vm) {
-  const struct discord_message *dis_msg;
-  struct discord *bot_client;
-  ficlDictionary *forth_dict = ficlVmGetDictionary(forth_vm);
-  ficlString tempString1;
-  ficlString tempString2;
-  FICL_STRING_SET_FROM_CSTRING(tempString1, "dis_msg");
-  FICL_STRING_SET_FROM_CSTRING(tempString2, "bot_client");
-  ficlVmExecuteWord(forth_vm, ficlDictionaryLookup(forth_dict, tempString2));
-  ficlVmExecuteWord(forth_vm, ficlDictionaryLookup(forth_dict, tempString1));
-  dis_msg = ficlStackPopPointer(forth_vm->dataStack);
-  bot_client = ficlStackPopPointer(forth_vm->dataStack);
+  const struct discord_message *dis_msg = get_dis_msg_from_forth(forth_vm);
+  struct discord *bot_client = get_client_from_forth(forth_vm);
   uint64_t dis_userid = ficlStackPopInteger(forth_vm->dataStack);
   log_info("Ban: %lu", dis_userid);
   discord_create_guild_ban(
